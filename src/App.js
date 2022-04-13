82020954c30e240dbd5e86c09b9d0227c98beccd
deanthvr <vdeanthony@computervisionaries.com>
2022-04-13 11:18:17 -0400

    front page update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* @flow*/
 
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import {
   CircleIconButton,
   Title,
@@ -13,14 +13,50 @@ import {
   Nav,
   NavItem,
   User,
-  Avatar
+  Avatar,
+  Button,
+  Card,
+  StatChange,
+  SectionSeparator
 } from "playbook-ui";
+
+const kpis = [
+  {
+    title: "Revenue",
+    value: 26,
+    increase: true,
+    active: true
+  },
+  {
+    title: "Orders",
+    value: 26,
+    increase: false,
+    active: false
+  },
+  {
+    title: "Revenue",
+    value: 26,
+    increase: false,
+    active: false
+  }
+];
 
 export default function App() {
   return (
     <div className="App">
-      <Background backgroundColor="light" paddingLeft="md" paddingY="xs">
-        <Flex orientation="row" spacing="between">
+      <Background
+        shadow="deep"
+        backgroundColor="white"
+        paddingLeft="md"
+        paddingY="xs"
+      >
+        <Flex
+          className="container"
+          orientation="row"
+          spacing="between"
+          justify="center"
+          align="center"
+        >
           <Flex justify="center" align="center" gap="2">
             <FlexItem marginRight="sm">
               <CircleIconButton icon="plus" variant="primary" />
@@ -52,6 +88,67 @@ export default function App() {
           </Flex>
         </Flex>
       </Background>
+      <SectionSeparator />
+      <Background backgroundColor="white" paddingBottom="none">
+        <Flex
+          className="container"
+          paddingLeft="xl"
+          paddingRight="md"
+          spacing="between"
+        >
+          <FlexItem>
+            <Nav link="#" marginLeft="sm" orientation="horizontal">
+              <NavItem active link="#" text="Dashboards" />
+              <NavItem link="#" text="Products" />
+              <NavItem link="#" text="In Progress" />
+              <NavItem link="#" text="Settings" />
+            </Nav>
+          </FlexItem>
+        </Flex>
+      </Background>
+      <Flex
+        className="container"
+        orientation="column"
+        padding="xl"
+        align="stretch"
+      >
+        <FlexItem>
+          <Caption text="Dashboard" />
+          <Title text="Commerce Dashboard" />
+        </FlexItem>
+        <FlexItem grow>
+          <Card padding="none">
+            <Card.Body padding="xs">
+              <Flex orientation="row" align="center" spacing="between">
+                <FlexItem>
+                  <Title text="Key Performance Indicators" tag="h4" size={4} />
+                </FlexItem>
+                <FlexItem>
+                  <CircleIconButton icon="ellipsis-h" variant="secondary" />
+                </FlexItem>
+              </Flex>
+            </Card.Body>
+            <SectionSeparator variant="card" />
+            <Card.Body>
+              <Flex orientation="row">
+                <FlexItem flex={3}>
+                  <Nav orientation="vertical">
+                    <NavItem active link="#">
+                      <Flex orientation="row">
+                        <Title text="Revenue" />
+                        <StatChange change="increase" value="26%" />
+                      </Flex>
+                    </NavItem>
+                    <NavItem link="#" text="Orders"></NavItem>
+                    <StatChange change="increase" value={26} />
+                  </Nav>
+                </FlexItem>
+                <FlexItem flex={8}></FlexItem>
+              </Flex>
+            </Card.Body>
+          </Card>
+        </FlexItem>
+      </Flex>
     </div>
   );
 }
